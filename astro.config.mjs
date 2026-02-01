4f52aadac131c46a325485f32cc24d57549ef27c
david yoder <david.yoder@live.com>
2026-02-01 18:48:47 -0500

    added css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // Astro config docs https://astro.build/config
 export default defineConfig({
-	// required to publish on GH Pages
-	site: 'https://omundy.github.io',
-	base: '/dig345-devlog',
-	integrations: [
-		// Startlight-specific config
-		starlight({
-			title: 'Owen\'s DevLog',
-			sidebar: [
-				{ label: 'Schedule', autogenerate: { directory: 'schedule' }, },
-			],
-		}),
-	],
+  // required to publish on GH Pages
+  site: "https://omundy.github.io",
+  base: "/dig345-devlog",
+  integrations: [
+    // Startlight-specific config
+    starlight({
+      title: "David's DevLog",
+      sidebar: [{ label: "Schedule", autogenerate: { directory: "schedule" } }],
+    }),
+  ],
+  customCss: [
+    "./src/styles/custom.css", // relative path to custom CSS file
+  ],
 });
